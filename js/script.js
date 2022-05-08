b9672df38cd5f4fe927aad48cabbb8bbdac4dd69
Julie Webb <julief.webb@gmail.com>
2022-05-08 15:17:07 -0400

    wip: got API image to display

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const URL = "https://api.harvardartmuseums.org/object/"
 const KEY = "apikey=e4224f80-5149-40c2-baf7-7dea1178f26e";
 const testURL = "https://api.harvardartmuseums.org/object/228649/?apikey=e4224f80-5149-40c2-baf7-7dea1178f26e"; //The Gare Saint-Lazare, Monet
 // ---> HTML Variables
-const $img = $('#image');
+const $img = $('#art');
 const $artist = $('#artist');
 const $title = $('#title');
 const $year = $('#year');
@@ -18,6 +18,10 @@ const $galleryText = $('#galleryText');
 function handleGetData(){
     $.ajax(testURL).then(function(data){
         console.log('art data is ready');
+        let src = data.images[0].iiifbaseuri + '/full/full/0/default.jpg';
+        $img.attr("src", src);
+        console.log(document.querySelector("#art"));
+        $artist.text(data.people[0].displayname);
         $galleryText.text(data.labeltext);
     }, function (error) {
         console.log('somethings is awry');

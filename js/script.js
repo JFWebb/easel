727ecbf03719fb87edfcdf6ad06224b3c1de85ec
Julie Webb <julief.webb@gmail.com>
2022-05-11 18:05:13 -0400

    wip: adjusted coordinates, heading, and pitch

--- js/script.js
+++ js/script.js
@@ -21,62 +21,54 @@ const coordinates = [
         title: "The Gare Saint-Lazare: Arrival of a Train",
         objectID: 228649,
         latlng: {
-            lat: 48.87654299083505,
-            lng: 2.3238306820761157,
+            lat: 48.876694707650074,
+            lng: 2.3237073184146277,
         },
-        heading: 4,
-        pitch: 10
+        // looking side-to-side
+        heading: -15,
+        // looking up & down
+        pitch: 10,
     },
     {
         title: "Nocturne in Grey and Gold: Chelsea Snow",
         objectID: 230417,
         latlng: {
-            lat: 51.48204449128982,
-            lng: -0.17413937371912416
+            lat: 51.482024320892386,
+            lng: -0.17400301461757903
         },
         heading: 34,
-        pitch: 10
-    },
-    {
-        title: "Nocturne in Grey and Gold: Chelsea Snow",
-        objectID: 230417,
-        latlng: {
-            lat: 51.48204449128982,
-            lng: -0.17413937371912416,
-        },
-        heading: 4,
         pitch: 10
     },
     {
         title: "Mardi Gras on the Boulevards",
         objectID: 229059,
         latlng: {
-            lat: 48.8712125970842,
-            lng: 2.344939078928708,
+            lat: 48.871200703105124,
+            lng: 2.344928095122574,
         },
-        heading: 4,
-        pitch: 10
+        heading: -54,
+        pitch: 15,
     },
     {
         title: "The Rehearsal",
         objectID: 303496,
         latlng: {
             lat: 48.87490106924117,
-            lng: 2.2782328346520906,
+            lng: 2.3394563623329425,
         },
-        heading: 4,
-        pitch: 10,
+        heading: 50,
+        pitch: 35,
     },
     {
         title: `Seated Figures, Study for "A Sunday Afternoon on the Island of La Grande Jatte"
         `,
         objectID: 229049,
         latlng: {
-            lat: 48.899723328699245,
-            lng: 2.3238306820761157,
+            lat: 48.89934596788276,
+            lng: 2.277605163112671,
         },
-        heading: 4,
-        pitch: 10,
+        heading: 110,
+        pitch: -10,
     }
 ]
 
@@ -97,7 +89,7 @@ function initMap(loc) {
     // The location
     const location = loc.latlng;
     console.log(loc.latlng);
-        // The map
+    // The map
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 4,
         center: location,
@@ -106,9 +98,9 @@ function initMap(loc) {
     const panorama = new google.maps.StreetViewPanorama(document.getElementById("pano"), {
         position: location,
         pov: {
-            // looking up/down
-            heading: loc.heading,
             // looking side-to-side
+            heading: loc.heading,
+            // looking up/down
             pitch: loc.pitch,
         },
     });
@@ -145,7 +137,7 @@ function handleGetData(event) {
     }, function (error) {
         console.log('somethings is awry');
         console.log(error);
-    });   
+    });
 };
 
 
